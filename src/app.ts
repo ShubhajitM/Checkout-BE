14cb23e7efd3717b459a8f7528bc92dd82c75603
Maaraa Codex Agent <codex-agent@maaraa.club>
2026-05-29 15:35:12 +0000

    codex: https://maaraa-team.atlassian.net/browse/KAN-5?atlOrigin=eyJpIjoiOTlhMmQ1NGI1YTZiNDE3ZGE4YzJhMTFkYzM0MzBiNjgiLCJwIjoiaiJ9
    - Add the necessary changes as mentioned in the given JIRA ticket
    
    Automated changes by Maaraa Codex Agent.
    Task ID: efab4cf6-e8e8-4c76-9562-b0c1f5f54297

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import { Item } from "./model/item";
-import { Sku } from "./model/sku";
 import { PricingRule } from "./pricing_rules/pricingRule";
 import { CheckOutImpl } from "./checkout/checkOutImpl";
 import { AppleTvPricingRule } from "./pricing_rules/appleTvPricingRule";
 import { SuperIpadPricingRule } from "./pricing_rules/superIpadPricingRule";
+import { MacbookProPricingRule } from "./pricing_rules/macbookProPricingRule";
 
 const pricingRuleList: PricingRule[] = [];
 
 const loadPricingRules = () => {
-    pricingRuleList.push(new AppleTvPricingRule())
-    pricingRuleList.push(new SuperIpadPricingRule())
+    pricingRuleList.push(new AppleTvPricingRule());
+    pricingRuleList.push(new SuperIpadPricingRule());
+    pricingRuleList.push(new MacbookProPricingRule());
 }
 
 loadPricingRules();
